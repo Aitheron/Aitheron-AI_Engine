2513d329ceb57b65ca8c8e8f56a4245b498d8885
Marlon <marlon@DESKTOP-0GB6S9L.localdomain>
2025-11-07 16:50:50 -0300

    feat: now streamlit runs in host 0.0.0.0 allowing it to be external acessed

--- run_server.sh
+++ run_server.sh
@@ -4,4 +4,4 @@ PYTHONPATH="$(pwd)/.." uvicorn main:app --reload --host 0.0.0.0 --port 8000 &
 
 cd ..
 cd frontend
-streamlit run app.py
+streamlit run app.py --server.address 0.0.0.0 --server.port 8501
